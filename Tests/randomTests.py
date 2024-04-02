e7343d817aea8555a17187805ef444c5d901abdc
Razi203 <30razi11@gmail.com>
2024-04-02 03:52:47 +0300

    + added shift key test + printing keys

--- Tests/randomTests.py
+++ Tests/randomTests.py
@@ -3,6 +3,8 @@ import string
 from ex5 import CaesarCipher, VigenereCipher, loadEncryptionSystem
 import time
 
+abc_count = ord('z') - ord('a') + 1
+
 def make_string():
     storage = string.whitespace[:3] + string.digits + string.ascii_letters + string.punctuation
     text = "".join([random.choice(storage) for i in range(random.randint(0,200))])
@@ -24,9 +26,17 @@ def main(test_count):
         original = make_string()
         encrypted = cypher.encrypt(original)
         decrypted = cypher.decrypt(encrypted)
+        updated = CaesarCipher(key)
+        if encrypted != updated.encrypt(original) or cypher.encrypt('a') != chr(ord('a') + (key%abc_count)):
+            Passed = False
+            print('_'*80)
+            print("#"*10 + " "*5 + "You are not shifting the keys correctly!" + " "*5 + "#"*10)
+            print('_'*80)
+            break
         if decrypted != original:
             Passed = False
             print("#"*10 + " "*5 + "FAILED Caesar" + " "*5 + "#"*10, end ='\n\n')
+            print(f"Key: ({key})\n")
             print("#"*10 + " "*5 + "Original Text" + " "*5 + "#"*10)
             print('_'*50)
             print(original)
@@ -56,6 +66,7 @@ def main(test_count):
         if decrypted != original:
             Passed = False
             print("#"*10 + " "*5 + "FAILED Vigenere" + " "*5 + "#"*10, end ='\n\n')
+            print(f"Keys: {keys}\n")
             print("#"*10 + " "*5 + "Original Text" + " "*5 + "#"*10)
             print('_'*50)
             print(original)
